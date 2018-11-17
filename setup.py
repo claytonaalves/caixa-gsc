9be58de0cd2d7565711b95dc5400d1f0d9888ea1
Clayton A. Alves <clayton.aa@gmail.com>
2018-11-17 14:44:57 -0300

    Aprimoramento na estrutura do projeto

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='gsc',
-      version='0.1',
-      description='Pacote de comunicação com Webservice de Gerenciamento de Serviços Caixa',
-      author='Clayton A. Alves',
-      author_email='clayton.aa@gmail.com',
-      url='https://github.com/claytonaalves/caixa-gsc',
-      packages=['gsc'],
+setup(
+    name='caixa.gsc',
+    version='0.2',
+    description='Biblioteca de comunicação com os webservices de Gerenciamento de Serviços da Caixa Econômica',
+    author='Clayton A. Alves',
+    author_email='clayton.aa@gmail.com',
+    url='https://github.com/claytonaalves/caixa.gsc',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'requests>=2.20.1',
+        'zeep>=3.1.0'
+    ]
 )
